ad63f7a02bec6b4babf7c5e40bd75f1d0b366d8f
Florin Buffet <suppiflo@yahoo.de>
2023-03-27 21:36:18 +0200

    Created custom User Model

--- SBEO/accounts/admin.py
+++ SBEO/accounts/admin.py
@@ -1,7 +1,70 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django import forms
+from django.core.exceptions import ValidationError
 
 from .models import User
 
+
+class UserChangeForm(forms.ModelForm):
+
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = User
+        fields = ['first_name','last_name','email','password','birth_date']
+
+class UserCreationForm(forms.ModelForm):
+
+    password1=forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2=forms.CharField(label='Confirm Password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['email','password','birth_date']
+    
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Passwords don't match")
+        return password2
+    
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password1'])
+        if commit:
+            user.save()
+        return user
+
+
+
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    list_display = ['last_name', 'first_name', 'email', 'is_admin']
+    list_filter = ['is_admin']
+    fieldsets = [
+        (None, {'fields': ['first_name', 'last_name', 'email', 'password']}),
+        ('Personal info', {'fields': ['birth_date', 'street', 'house_number', 'postal_code', 'city', 'country']}),
+        ('Permissions', {'fields': ['is_admin']}),
+    ]
+    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
+    add_fieldsets = [
+        (
+            None,
+            {
+                'classes': ['wide'],
+                'fields': ['email', 'birth_date', 'password1', 'password2'],
+            },
+        ),
+    ]
+    search_fields = ['email', 'first_name', 'last_name']
+    ordering = ['last_name', 'first_name']
+    filter_horizontal = []
+
 # Register your models here.
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
--- SBEO/accounts/models.py
+++ SBEO/accounts/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django_countries.fields import CountryField
@@ -6,15 +6,55 @@ from django_countries.fields import CountryField
 
 # Create your models here.
 
-class User(AbstractUser):
+class UserManager(BaseUserManager):
+    def create_user(self, email, birth_date, mobile, street, house_number, postal_code, city, country, password=None):
+        if not email:
+            raise ValueError("User must have an email adress")
+
+        user = self.model(
+            email=self.normalize_email(email),
+            birth_date=birth_date,
+            mobile=mobile,
+            street=street,
+            house_number=house_number,
+            postal_code=postal_code,
+            city=city,
+            country=country,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, birth_date, mobile, street, house_number, postal_code, city, country, password=None):
+        superuser = self.create_user(
+            email,
+            password=password,
+            birth_date=birth_date,
+            mobile=mobile,
+            street=street,
+            house_number=house_number,
+            postal_code=postal_code,
+            city=city,
+            country=country,
+        )
+
+        superuser.is_admin = True
+        superuser.save(using=self._db)
+        return superuser
+
+
+class User(AbstractBaseUser):
     email = models.EmailField(
         verbose_name=_('E-Mail adress'),
         max_length=50,
         unique=True)
+    first_name = models.CharField(max_length=50, verbose_name=_('First Name'))
+    last_name = models.CharField(max_length=50, verbose_name=_('Last Name'))
     birth_date = models.DateField(verbose_name=_('Date of Birth'), null=True)
     mobile = models.CharField(
         max_length=16,
-        verbose_name=_('Mobile number'), blank=True)
+        verbose_name=_('Mobile number'))
     allow_Reselection = models.BooleanField(
         default=True,
         verbose_name=_('Allow reselection to similar tournament'))
@@ -32,5 +72,22 @@ class User(AbstractUser):
         verbose_name=_('City'))
     country = CountryField(
         verbose_name=_('Country'))
-    USERNAME_FIELD='email'
-    REQUIRED_FIELDS=['birth_date']
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['birth_date', 'mobile', 'street',
+                       'house_number', 'postal_code', 'city', 'country']
+    is_admin = models.BooleanField(default=False, verbose_name=_('Is Admin'))
+
+    @property
+    def is_staff(self):
+        return self.is_admin
+    
+    def has_module_perms(self, app_label):
+        return True
+
+    def has_perm(self, app_label):
+        return True
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email

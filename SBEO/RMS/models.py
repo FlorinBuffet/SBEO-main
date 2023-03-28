146110e53b80ca745521f2ebf4efccffbf86bf9c
Florin Buffet <suppiflo@yahoo.de>
2023-03-28 10:22:56 +0200

    runned isort

--- SBEO/RMS/models.py
+++ SBEO/RMS/models.py
@@ -1,7 +1,7 @@
+from django.conf import settings
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django_countries.fields import CountryField
-from django.conf import settings
 
 # Create your models here.
 
--- SBEO/accounts/models.py
+++ SBEO/accounts/models.py
@@ -3,7 +3,6 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django_countries.fields import CountryField
 
-
 # Create your models here.
 
 class UserManager(BaseUserManager):
